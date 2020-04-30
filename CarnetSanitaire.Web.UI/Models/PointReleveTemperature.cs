a054fd9b5d0ef47b0c41826208c08e8cf7b183e2
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-04-30 13:50:21 +0200

    Modification de RelevePoint Temperature pour y inclure l'idTypePoint

--- CarnetSanitaire.Web.UI/Models/PointReleveTemperature.cs
+++ CarnetSanitaire.Web.UI/Models/PointReleveTemperature.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace CarnetSanitaire.Web.UI.Models
         public int Id { get; set; }
         public string Nom { get; set; }
         public string Localisation { get; set; }
+        public int TypePointId { get; set; }
+        [ForeignKey("TypePointId")]
         public TypePoint TypePoint { get; set; }
         public List<ReleveTemperature> ReleveTemperatures { get; set; }
         public bool Statut { get; set; }
